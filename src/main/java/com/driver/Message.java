9f844e100e35a8b7cd94915efa0d817548de10c0
Your Name <sabavathnaveen951@gmail.com>
2023-03-15 12:41:38 +0530

    sol3

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -13,7 +13,7 @@ public class Message {
 
     }
 
-    public Message(int id, String content, Date timestamp) {
+    public Message(int id, String content) {
         this.id = id;
         this.content = content;
         this.timestamp = timestamp;
